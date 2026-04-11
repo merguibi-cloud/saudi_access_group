b187c850b70f225e80f0848019ccc04282d5a1de
merguibi-cloud <m.erguibi@elite-society.fr>
2026-04-11 16:37:02 +0100

    timeline

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,6 +2,7 @@ import { HeroSection } from "../components/HeroSection";
 import { ProblemAccessSection } from "../components/ProblemAccessSection";
 import { SolutionSAGSection } from "../components/SolutionSAGSection";
 import { ServicesSection } from "../components/ServicesSection";
+import { OnboardingTimeline } from "../components/OnboardingTimeline";
 import { Vision2030Section } from "../components/Vision2030Section";
 import { ImmersionProgramSection } from "../components/ImmersionProgramSection";
 import { FoundersSection } from "../components/FoundersSection";
@@ -14,6 +15,7 @@ export default function Home() {
       <ProblemAccessSection />
       <SolutionSAGSection />
       <ServicesSection />
+      <OnboardingTimeline />
       <Vision2030Section />
       <ImmersionProgramSection />
       <FoundersSection />
